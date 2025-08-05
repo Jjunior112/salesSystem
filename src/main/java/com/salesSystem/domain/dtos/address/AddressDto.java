1a2d92b05d4a7819ccdd3e94b9ef31ef4cc160ca
jjunior112 <juarezgcjr@gmail.com>
2025-08-05 04:13:09 -0300

    feat: create do dto de endereço

---
+++ src/main/java/com/salesSystem/domain/dtos/address/AddressDto.java
@@ -0,0 +1,25 @@
+package com.salesSystem.domain.dtos.address;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+public record AddressDto(
+        @NotBlank
+        String street,
+
+        @NotBlank
+        String neighborhood,
+
+        @NotBlank
+        @Pattern(regexp = "\\d{8}")
+        String zipCode,
+
+        @NotBlank
+        String city,
+
+        @NotBlank
+        String state,
+
+        String complement,
+
+        String number
+) {}
