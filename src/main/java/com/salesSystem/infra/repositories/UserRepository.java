ebbf1a813dfb4d3a8dd3b398c0f12c9cbd1d540f
jjunior112 <juarezgcjr@gmail.com>
2025-08-05 04:15:52 -0300

    feat: create do repository de usuários

---
+++ src/main/java/com/salesSystem/infra/repositories/UserRepository.java
@@ -0,0 +1,16 @@
+package com.salesSystem.infra.repositories;
+
+import com.salesSystem.domain.models.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    UserDetails findByEmail(String login);
+
+    Page<User> findAllByIsActiveTrue(Pageable pagination);
+}
