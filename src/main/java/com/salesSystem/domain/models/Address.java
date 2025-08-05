0c1d925a1aee60c37ff93730460672ad64971f29
jjunior112 <juarezgcjr@gmail.com>
2025-08-05 04:12:58 -0300

    feat: create da classe de endereço

---
+++ src/main/java/com/salesSystem/domain/models/Address.java
@@ -0,0 +1,58 @@
+package com.salesSystem.domain.models;
+
+import com.salesSystem.domain.dtos.address.AddressDto;
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Embeddable
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+
+    private String street;
+    private String neighborhood;
+    private String zipCode;
+    private String city;
+    private String state;
+    private String complement;
+    private String number;
+
+    public Address(AddressDto data) {
+        this.street = data.street();
+        this.neighborhood = data.neighborhood();
+        this.number = data.number();
+        this.complement = data.complement();
+        this.city = data.city();
+        this.zipCode = data.zipCode();
+        this.state = data.state();
+    }
+
+    public void updateInformation(AddressDto data) {
+        if (data.street() != null) {
+            this.street = data.street();
+        }
+        if (data.neighborhood() != null) {
+            this.neighborhood = data.neighborhood();
+        }
+        if (data.number() != null) {
+            this.number = data.number();
+        }
+        if (data.complement() != null) {
+            this.complement = data.complement();
+        }
+        if (data.city() != null) {
+            this.city = data.city();
+        }
+        if (data.zipCode() != null) {
+            this.zipCode = data.zipCode();
+        }
+        if (data.state() != null) {
+            this.state = data.state();
+        }
+    }
+}
