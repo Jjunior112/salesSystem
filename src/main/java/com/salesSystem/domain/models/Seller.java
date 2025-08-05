9467db81e8b8f9540da381242cabe2a250435bc6
jjunior112 <juarezgcjr@gmail.com>
2025-08-05 04:15:16 -0300

    feat: create da classe de vendedor

---
+++ src/main/java/com/salesSystem/domain/models/Seller.java
@@ -0,0 +1,51 @@
+package com.salesSystem.domain.models;
+
+import com.salesSystem.domain.enums.UserRole;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "sellers")
+@Getter
+@Setter
+
+public class Seller extends User {
+
+    public Seller() {
+        super();
+    }
+
+    private String firstName;
+
+    private String lastName;
+
+    private String phone;
+
+    @Column(unique = true, nullable = false)
+    private String document;
+
+    @Embedded
+    private Address address;
+
+
+    public Seller(@NotBlank String firstName, @NotBlank String lastName, @NotBlank String phone, @NotBlank String document, @NotNull Address address, @Email String email, String password, UserRole role) {
+        super(email, password, role);
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.document = document;
+        this.address = address;
+    }
+
+
+}
