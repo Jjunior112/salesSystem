049971c88ca1f6bbe5664f5ef72088568ffb235b
jjunior112 <juarezgcjr@gmail.com>
2025-08-05 04:15:31 -0300

    feat: create do repository de vendedor

---
+++ src/main/java/com/salesSystem/infra/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.salesSystem.infra.repositories;
+
+import com.salesSystem.domain.models.Seller;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface SellerRepository extends JpaRepository<Seller, UUID> {
+}
