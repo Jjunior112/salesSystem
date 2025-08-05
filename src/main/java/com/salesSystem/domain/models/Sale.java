ae8de948404804b6092b595e2366d44daa77b010
jjunior112 <juarezgcjr@gmail.com>
2025-08-05 04:21:37 -0300

    feat: create de classe para vendas

---
+++ src/main/java/com/salesSystem/domain/models/Sale.java
@@ -0,0 +1,54 @@
+package com.salesSystem.domain.models;
+
+import com.salesSystem.domain.dtos.sale.SaleRegisterDto;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity(name = "sales")
+@Table(name = "sales")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Sale {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "seller_id")
+    private Seller seller;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "buyer_id")
+    private Client buyer;
+
+    private LocalDateTime timestamp = LocalDateTime.now();
+
+    @ElementCollection
+    @CollectionTable(name = "client_cart_items", joinColumns = @JoinColumn(name = "client_id"))
+    private List<CartItem> cartItems = new ArrayList<>();
+
+    public void addItemToCart(Product product, int quantity) {
+        CartItem item = new CartItem(product.getId(), product.getProductName(), product.getPrice(), quantity);
+        cartItems.add(item);
+    }
+
+    public BigDecimal getCartTotal() {
+        return cartItems.stream()
+                .map(CartItem::getSubtotal)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    public Sale(Client client, Seller seller, List<CartItem> cart) {
+        this.seller = seller;
+        this.buyer = client;
+        this.cartItems = cart;
+    }
+}
