8b5b61cd4a5146c756d6c5a6b728b16e4460244c
jjunior112 <juarezgcjr@gmail.com>
2025-08-22 03:59:30 -0300

    test: create de classe de teste para clientService

---
+++ src/test/java/com/salesSystem/application/services/ClientServiceTest.java
@@ -0,0 +1,84 @@
+package com.salesSystem.application.services;
+
+import com.salesSystem.domain.dtos.address.AddressDto;
+import com.salesSystem.domain.dtos.client.ClientRegisterDto;
+import com.salesSystem.domain.models.Address;
+import com.salesSystem.domain.models.Client;
+import com.salesSystem.infra.repositories.ClientRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+class ClientServiceTest {
+
+    @Mock
+    private ClientRepository repository;
+
+    @Autowired
+    @InjectMocks
+    private ClientService clientService;
+
+    private Client client;
+
+
+    @BeforeEach
+    void setup() {
+        MockitoAnnotations.openMocks(this);
+
+        AddressDto addressDto = new AddressDto("rua", "bairro", "12546888", "cidade", "estado", "complemento", "123");
+
+        Address address = new Address(addressDto);
+
+        ClientRegisterDto clientRegisterDto = new ClientRegisterDto("firstname", "lastname", "12123456789", "111.222.555-55", address);
+
+        client = new Client(clientRegisterDto);
+
+    }
+
+    @Test
+    void createClientCase1() {
+        //arrange
+
+        AddressDto addressDto = new AddressDto("rua", "bairro", "12546888", "cidade", "estado", "complemento", "123");
+
+        Address address = new Address(addressDto);
+
+        ClientRegisterDto clientRegisterDto = new ClientRegisterDto("firstname", "lastname", "12123456789", "111.222.555-55", address);
+
+        //act
+
+        Client result = clientService.createClient(clientRegisterDto);
+
+        //assert
+
+        verify(repository, times(1)).save(any());
+
+        assertEquals(clientRegisterDto.firstName(), result.getFirstName(),"O nome do cliente retornado deve ser igual ao enviado");
+    }
+
+    @Test
+    void findClientByIdCase1() {
+        //arrange
+
+        UUID id = UUID.randomUUID();
+
+        System.out.println(id);
+
+        //act
+
+        Client result = clientService.findClientById(id);
+
+        verify(repository, times(1)).getReferenceById(any());
+
+    }
+}
