df0a1475c1a65ba8d279b7df76bd3cd4d4c700f8
jjunior112 <juarezgcjr@gmail.com>
2025-08-22 04:09:10 -0300

    test: nomes para os metodos de teste do clienteService

--- src/test/java/com/salesSystem/application/services/ClientServiceTest.java
+++ src/test/java/com/salesSystem/application/services/ClientServiceTest.java
@@ -6,6 +6,7 @@ import com.salesSystem.domain.models.Address;
 import com.salesSystem.domain.models.Client;
 import com.salesSystem.infra.repositories.ClientRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -46,6 +47,7 @@ class ClientServiceTest {
     }
 
     @Test
+    @DisplayName("Deveria registrar o cliente corretamente")
     void createClientCase1() {
         //arrange
 
@@ -67,6 +69,7 @@ class ClientServiceTest {
     }
 
     @Test
+    @DisplayName("Deveria retornar cliente corretamente")
     void findClientByIdCase1() {
         //arrange
 
