88bd9bbed3b1836787ef402b75423dcab2879345
jjunior112 <juarezgcjr@gmail.com>
2025-08-22 03:55:18 -0300

    refactor: ajuste no dto de resposta do produto para conter a descrição

--- src/main/java/com/salesSystem/domain/dtos/product/ProductListDto.java
+++ src/main/java/com/salesSystem/domain/dtos/product/ProductListDto.java
@@ -5,11 +5,16 @@ import com.salesSystem.domain.models.Product;
 
 import java.math.BigDecimal;
 
-public record ProductListDto(Long id, String productName, ProductCategory productCategory, String brand,
+public record ProductListDto(Long id,
+
+                             String productName,
+                             ProductCategory productCategory,
+                             String productDescription,
+                             String brand,
                              Integer balance,
                              BigDecimal price) {
 
     public ProductListDto(Product product) {
-        this(product.getId(), product.getProductName(), product.getProductCategory(), product.getBrand(), product.getBalance(), product.getPrice());
+        this(product.getId(), product.getProductName(), product.getProductCategory(), product.getProductDescription(), product.getBrand(), product.getBalance(), product.getPrice());
     }
 }
